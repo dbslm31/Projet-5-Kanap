f26285d095105fd145251a925f683d72eda9098a
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2022-08-19 18:41:32 +0100

    fetch_api+card_innerhtml

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,30 @@
-getProducts()
+fetch("http://localhost:3000/api/products")
+    .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function (products) {
+        console.log(products);
 
-async function getProducts() {
-    let products = await fetch("http://localhost:3000/api/products");
-    console.log("Les produits ont été importés")
-    return product.js;
-} 
+        let items = document.getElementById('items');
+
+        for (let i = 0; i < products.length; i++) {
+            const itemsCards = `<a href="./product.html?${products[i]._id}"> 
+                <article> 
+                <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+                <h3 class="productName">${products[i].name}</h3>
+                <p class="productDescription">${products[i].description}</p>
+                </article>
+                </a>`;
+            items.innerHTML = itemsCards;
+        }
+
+    })
+    .catch(function (err) {
+        // Une erreur est survenue
+    });
+
+
+
+
