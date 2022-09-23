9fe8ec4cf02bcfd4e9f03fd7ec7815402d72c408
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2022-09-23 19:35:33 +0200

    quantity_productjs_fix

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,43 @@
-// Récupération des infos du localStorage
-
-let addedProducts = JSON.parse(localStorage.getItem("product"));
 
 
 
 
+// Récupération des données du localStorage et création du tableau
+let productArray = [JSON.parse(localStorage.getItem("product"))];
+console.log("productArray", productArray);
+
+// si le panier est vide
+if (productArray == null || productArray.length == 0) {
+    console.log("Le panier est vide")
+    // le h1 affiche "Votre panier est vide"
+    let cartTitle = document.querySelector("#cartAndFormContainer >h1")
+    cartTitle.innerText += " est vide"
+    // si le panier contient des produits   
+
+
+} else {
+    console.log('le panier contient des produits')
+    // on affiche les infos du produits dans le panier
+    /*document.getElementById("cart__items").innerHTML = `<article class="cart__item" data-id="${productData._id}" data-color="${productData._color}">
+    <div class="cart__item__img">
+      <img src="${productData.imageUrl}" alt="${productData.altTxt}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${productData.name}</h2>
+        <p>${productDatacolorss}</p>
+        <p>${productData.price}</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>`*/
+
+}
--- front/js/product.js
+++ front/js/product.js
@@ -33,7 +33,7 @@ const fetchProduct = async () => {
             <option value="${productData.colors[i]}">${productData.colors[i]}</option>`;
             }
 
-            addToCart(productData);
+
         });
 
 };
@@ -41,59 +41,101 @@ const fetchProduct = async () => {
 fetchProduct();
 
 
-const addToCart = () => {
-
-    // Definition de la variable cartButton
-    let cartButton = document.getElementById("addToCart");
-
-    // Ecoute de l'évènement click
-    cartButton.addEventListener("click", () => {
-
-        // Récupération de ce que contient le local storage
-        let productArray = JSON.parse(localStorage.getItem("product"))
 
 
-        // Définition des variables color et quantity
-        let color = document.getElementById("colors").value;
-        let quantity = Number(document.getElementById("quantity").value);
-        console.log('color', color);
-        console.log('quantity', quantity)
+// Definition de la variable cartButton
+let cartButton = document.getElementById("addToCart");
 
-        //Création objet JS avec les infos du produit
-        let productInfos = {
-            id: id,
-            color: color,
-            quantity: quantity
+// Ecoute de l'évènement click
+cartButton.addEventListener("click", () => {
 
-        }
+    // Récupération de ce que contient le local storage
+    let productArray = JSON.parse(localStorage.getItem("product"))
 
-        // si la couleur ou la quantité n'est pas selectionnée
-        if (color == "" || quantity == "") {
-            alert("Veuillez spécifier la couleur et la quantité");
 
-        } else if (productArray == null) {
-            productArray = [];
-            productArray.push(productInfos)
-            console.log("productArray", productArray);
+
+    // Définition des variables color et quantity
+    let color = document.getElementById("colors").value;
+    let quantity = Number(document.getElementById("quantity").value);
+    console.log('color', color);
+    console.log('quantity', quantity)
+
+
+    //Création objet JS avec les infos du produit
+    let productInfos = {
+        id: id,
+        color: color,
+        quantity: quantity
+
+    }
+
+    // si la couleur ou la quantité n'est pas selectionnée
+    if (color == "" || quantity == "") {
+        alert("Veuillez spécifier la couleur et la quantité");
+
+        // si le panier est vide on push le nouveau produit 
+    } else if (productArray == null) {
+        productArray = [];
+        productArray.push(productInfos)
+        console.log("productArray", productArray);
+        localStorage.setItem("product", JSON.stringify(productArray));
+
+
+        // si le panier contient déjà un produit
+    } else {
+
+        const findProduct = productArray.find(product => product.id === id && product.color === color)
+        console.log(findProduct)
+        if (findProduct != undefined) {
+
+            let newQuantity = Number(findProduct.quantity);
+            newQuantity += quantity;
+            findProduct.quantity = newQuantity;
             localStorage.setItem("product", JSON.stringify(productArray));
 
-
         } else {
-            for (i = 0; i < productArray.length; i++) {
-                if (productArray[i].id === id && productArray[i].color === color) {
-                    let newQuantity = Number(productArray[i].quantity);
-                    newQuantity += quantity;
-                    productArray[i].quantity = newQuantity;
-                    localStorage.setItem("product", JSON.stringify(productArray));
-                    productArray = JSON.parse(localStorage.getItem("product"))
-                } else {
-                    productArray.push(productInfos)
-                    localStorage.setItem("product", JSON.stringify(productArray));
-                }
-            }
+            productArray.push(productInfos)
+            localStorage.setItem("product", JSON.stringify(productArray));
         }
 
 
-    });
 
-}
+
+
+
+
+
+
+
+
+        /*const productArrayLength = productArray.length
+        let isProductHere = false
+
+        for (let i = 0; i < productArrayLength; i++) {
+            console.log(i);
+            let newQuantity = Number(productArray[i].quantity);
+            
+
+            // si les produits ont le même id & la même couleur on incrémente la quantité
+            if (productArray[i].id === id && productArray[i].color === color) {
+                isProductHere = true
+                console.log("block if")
+                newQuantity += quantity;
+                productArray[i].quantity = newQuantity;
+                localStorage.setItem("product", JSON.stringify(productArray));
+                productArray = JSON.parse(localStorage.getItem("product"))
+
+
+                // sinon on push produit en tant que nouveau produit dans le panier
+            } 
+
+        }
+        if (isProductHere == false) {
+            product
+        }
+        */
+    }
+});
+
+
+
